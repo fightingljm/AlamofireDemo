f7dc6ce135fd8485c6f561cd9cf8dd071e94ea27
刘金萌 <1321839748ljm@gmail.com>
2019-07-23 17:14:15 +0800

    Alamofire post get 使用

--- AlamofireDemo/ViewController.swift
+++ AlamofireDemo/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Alamofire.request("http://httpbin.org/get",method: .get).responseJSON { (response) in
+            guard let result = response.result.value else {
+                print(response.result.error ?? "default value")
+                return
+            }
+            print(result)
+        }
+        
+        Alamofire.request("http://httpbin.org/post",method: .post,parameters: ["name":"ljm"]).responseJSON { (response) in
+            guard let result = response.result.value else {
+                print(response.result.error ?? "default value")
+                return
+            }
+            print(result)
+        }
+        
     }
 
 
